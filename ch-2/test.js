cb8cc21493479a48345fa21c15ec3a1d8e496395
Oluwasetemi Ojo <setemiojo@gmail.com>
2023-08-22 19:03:59 -0500

    update the tests

--- ch-2/test.js
+++ ch-2/test.js
@@ -4,11 +4,11 @@ const axios = require('axios');
 const { countEvenNumbersWithin } = assignment;
 const maybe = countEvenNumbersWithin === undefined ? test.skip : test;
 
-maybe('Challenge - 2 : countEvenNumbersWithin can count even numbers in array of numbers', async () => {
+maybe('Challenge - 2 : countEvenNumbersWithin can count, sum, arrayOfEvenNumbers from 1 to destination', async () => {
     const URL = 'https://randomapi.com/api/3ba71dfdc0144abba3452e039f210123';
     const { data: { results } } = await axios.get(URL);
     const [randomTestData] = results;
     const { value, correctFunction } = randomTestData;
 
-    expect(countEvenNumbersWithin(value)).toBe(correctFunction);
+    expect(countEvenNumbersWithin(value)).toStrictEqual(correctFunction);
 });
